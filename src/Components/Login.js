8fdabf9131f65043717c5d44f8946b81507d43b9
Rixon <rixonraj16@gmail.com>
2023-07-25 18:26:26 +0530

    MM forget pass hide

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -74,13 +74,6 @@ function Login() {
                                                 <label className="form-label" htmlFor="form2Example22">Password</label>
                                             </div>
 
-                                            <div className="text-center pt-1 mb-5 pb-1">
-                                                <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type={"submit"}>Log
-                                                    in</button>
-                                                    <br/>
-                                                <a className="text-muted" href="#!">Forgot password?</a>
-                                            </div>
-
                                             <div className="d-flex align-items-center justify-content-center pb-4">
                                                 <p className="mb-0 me-2">Don't have an account?</p>
                                                 <Link to={`/register`}className="btn btn-outline-danger">Create new</Link>
