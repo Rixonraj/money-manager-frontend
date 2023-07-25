6b868408f2aa6620e4345f5de11145b50155eb35
Rixon <rixonraj16@gmail.com>
2023-07-25 18:27:21 +0530

    MM hide unwanted elem

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -80,14 +80,6 @@ function Register() {
 
                                     </div>
                                 </div>
-                                <div className="col-lg-12 d-flex align-items-center gradient-custom-2">
-                                    <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                                        <h4 className="mb-4">We are more than just a company</h4>
-                                        <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
